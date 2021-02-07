eaf2d07d7df853dda7f9c36c59aff891ee4d2ac5
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-02-07 13:04:34 -0500

    2nd commit with code. Unfinished

--- AlifLesson9HW06.02/Program.cs
+++ AlifLesson9HW06.02/Program.cs
@@ -6,7 +6,123 @@ namespace AlifLesson9HW06._02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] num = new int[] { 5, 2, 6, 9, 4, 2 };
+            string[] str = new string[] { "ant", "bison", "camel", "duck", "elephant" };
+
+            //Console.WriteLine(ArrayHelper.Pop<int>(ref num));
+            //Console.WriteLine(ArrayHelper.Pop<string>(ref str));
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    Console.WriteLine(ArrayHelper.Slice<string>(str, 2, 4));
+            //}
+            //Console.WriteLine(ArrayHelper.Slice<string>(str, 2, 4));
+
+            var beginIndex = Convert.ToInt32(Console.ReadLine());
+            //var endIndex = Convert.ToInt32(Console.ReadLine());
+
+            foreach (string i in ArrayHelper.Slice<string>(str, beginIndex))
+            {
+
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+
+    public static class ArrayHelper
+    {
+
+        //Pop Methods
+        public static T Pop<T>(ref T[] lastElement)
+        {
+            T arrLastElement = lastElement[lastElement.Length - 1];
+            return arrLastElement;
+        }
+
+        //Push Methods
+        public static int Push<T>(ref T[] addToLast, T updatedArray)
+        {
+            Array.Resize(ref addToLast, addToLast.Length + 1);
+            addToLast[addToLast.Length - 1] = updatedArray;
+            return addToLast.Length;
+        }
+
+        //Shift Methods
+        public static T Shift<T>(ref T[] shiftElement)
+        {
+            T shift = shiftElement[0];
+            for (int i = 0; i < shiftElement.Length - 1; i++)
+            {
+                shiftElement[i] = shiftElement[i + 1];
+            }
+            Array.Resize(ref shiftElement, shiftElement.Length - 1);
+            return shift;
+        }
+
+        //Unshift Methods
+        public static int UnShift<T>(ref T[] addToFirst, T toFirstElement)
+        {
+            Array.Resize(ref addToFirst, addToFirst.Length + 1);
+            for (int i = addToFirst.Length - 1; i >= 1; i--)
+            {
+                addToFirst[i] = addToFirst[i - 1];
+            }
+            addToFirst[0] = toFirstElement;
+            return addToFirst.Length - 1;
+        }
+        //Slice Method
+        public static T[] Slice<T>(T[] arr)
+        {
+            return arr;
+        }
+
+        public static T[] Slice<T>(T[] arr, int beginIndex)
+        {
+            if (beginIndex > arr.Length)
+            {
+                return default;
+            }
+
+            if (beginIndex < 0)
+            {
+                beginIndex = arr.Length - Math.Abs(beginIndex);
+            }
+
+            int slice = arr.Length - beginIndex;
+            T[] newArr = new T[slice];
+            for (int i = 0; i < slice; i += 1)
+            {
+                newArr[i] = arr[beginIndex + i];
+            }
+            return newArr;
+        }
+
+        public static T[] Slice<T>(T[] arr, int beginIndex, int endIndex)
+        {
+            if (beginIndex > arr.Length)
+            {
+                T[] newAr = new T[0];
+                return newAr;
+            }
+
+            if (beginIndex < 0)
+            {
+                beginIndex = arr.Length - Math.Abs(beginIndex);
+            }
+
+            if (endIndex < 0)
+            {
+                endIndex = arr.Length - Math.Abs(endIndex);
+            }
+
+            int slice = endIndex - beginIndex;
+            T[] newArr = new T[slice];
+            for (int i = 0; i < slice; i += 1)
+            {
+                newArr[i] = arr[beginIndex + i];
+            }
+            return newArr;
         }
     }
 }
